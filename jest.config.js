8db4152c0bf6942620c44e442dcc619989212ba7
gotomts <mh.goto.web@gmail.com>
2020-10-31 10:24:01 +0900

    jest用に設定調整

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  verbose: true,
-  collectCoverage: true,
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
+  "roots": [
+    "<rootDir>/src"
   ],
-};
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+}
