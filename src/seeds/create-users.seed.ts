ebfa9442a32a95f584e393f23c924d996e663180
gotomts <mh.goto.web@gmail.com>
2020-11-07 01:29:58 +0900

    コメントアウト削除

--- src/seeds/create-users.seed.ts
+++ src/seeds/create-users.seed.ts
@@ -1,7 +1,5 @@
 import { Seeder, Factory } from 'typeorm-seeding';
-// import { createConnection } from 'typeorm';
 import { User } from '../entity/User';
-// import option from '../common/db-option';
 
 export default class CreateUsers implements Seeder {
   run = async (factory: Factory): Promise<void> => {
